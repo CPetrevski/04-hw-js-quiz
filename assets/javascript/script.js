4b29fe792e426035f9a780a0e4a5329202c4ed35
Cpetrevski <c_petrevski@hotmail.com>
2021-09-16 11:23:10 +1000

    Major Update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,27 @@
 var startButton = document.querySelector(".start-btn button");
 var cardQuestions = document.querySelector(".card");
 var nextButton = document.querySelector(".next-btn");
-var option_list = document.querySelector(".option_list");
+var optionList = document.querySelector(".option-list");
+var timeLeft = document.querySelector(".seconds-left");
+var resultBox = document.querySelector(".result");
+var submitButton = resultBox.querySelector(".submit .submit-btn");
 
 
 var highScore = 0;
 var questionNumber = 0;
 var questionPosition = 1;
+var counter = 0;
 
 function clickStart() {
     startButton.style.display = "none";
     cardQuestions.style.visibility = "visible";
+    resultBox.style.visibility = "hidden";
 }
  
 startButton.addEventListener("click", function() {
     clickStart();
     showQuestions(0);
+    startTimer(120)
     
 });
 
@@ -27,6 +33,7 @@ nextButton.addEventListener("click", function() {
     }
     else {
         console.log("Finished");
+        results();
     }
         
 });
@@ -34,7 +41,7 @@ nextButton.addEventListener("click", function() {
 
 function showQuestions(index) {
     var quest = document.querySelector(".question-text");
-    var optionList = document.querySelector(".option-list");
+    
     var questionTag = "<span>"+ questions[index].question + "</span";
     var optionTag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
                         + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
@@ -53,4 +60,52 @@ function showQuestions(index) {
 function optionSelected(answer) {
     var userAnswer = answer.textContent;
     var correctAnswer = questions[questionNumber].answer;
+    var allOptions = optionList.children.length;
+
+    if (userAnswer == correctAnswer) {
+        answer.classList.add("correct")
+        console.log("Correct");
+        highScore += 10;
+    }
+    else {
+        answer.classList.add("incorrect");
+        console.log("Incorrect");
+                
+    }
+    for (var i = 0; i < allOptions; i++) {
+        optionList.children[i].classList.add("disabled");
+        
+    }
+    
+}
+
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+    
+    function timer() {
+        timeLeft.textContent = time;
+        time--;
+    }
+    if (time < 0) {
+        clearInterval(counter);
+        timeLeft.textContent = "00";
+    }
+}
+
+function results() {
+    cardQuestions.style.visibility = "hidden";
+    resultBox.style.visibility = "visible";
+    var scoreText = resultBox.querySelector(".score");
+    var scoreTag = "<span>Your Score was: " + highScore + "</span>";
+    scoreText.innerHTML = scoreTag;
+}
+
+submitButton.addEventListener("click", function() {
+    submitResults();
+    window.location.href = "highscore.html";
+    
+});
+
+function submitResults() {
+
 }
